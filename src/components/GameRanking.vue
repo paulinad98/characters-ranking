bde007091093671b2a10814f02fb3db60333e802
Paulina <paulinaklempka@gmail.com>
2023-08-26 17:48:05 +0200

    Add GameRanking component

---
+++ src/components/GameRanking.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { Character } from '@/types/types'
+
+defineProps<{
+  charactersRanking: (Character | null)[]
+}>()
+</script>
+
+<template>
+  <ol class="game-ranking">
+    <li :key="i" v-for="(character, i) in charactersRanking" class="ranking-wrapper">
+      <span class="ranking-place">{{ i + 1 }}</span>
+      <button @click="$emit('select', i)" class="ranking-btn" :disabled="!!character">
+        <img
+          v-if="!!character"
+          :src="character.img"
+          :alt="character.name"
+          class="ranking-character-img"
+        />
+      </button>
+    </li>
+  </ol>
+</template>
+
+<style scoped>
+.game-ranking {
+  --ranking-theme: rgb(var(--raspberry-rose-rgb));
+
+  position: absolute;
+  top: 0;
+  left: 8%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 12px;
+}
+
+.ranking-wrapper {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+.ranking-btn {
+  width: 80px;
+  height: 45px;
+  overflow: hidden;
+  border-radius: 12px;
+  background-color: rgb(var(--white-mute-rgb));
+  border: 2px solid transparent;
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+.ranking-btn:hover {
+  border: 2px solid var(--ranking-theme);
+}
+
+.ranking-character-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.ranking-place {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 30px;
+  width: 30px;
+  border-radius: 4px;
+  background-color: var(--ranking-theme);
+  color: rgb(var(--white-mute-rgb));
+}
+</style>
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,7 @@
 export type ThemeColors = 'ash-grey' | 'pale-dogwood' | 'raisin-black' | 'raspberry-rose' | 'azure'
+
+export type Character = {
+  id: number
+  img: string
+  name: string
+}
