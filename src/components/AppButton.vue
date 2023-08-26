363604ea63551cec78106170dc9dd8dd190a75fb
Paulina <paulinaklempka@gmail.com>
2023-08-26 08:56:31 +0200

    Clean comments

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -16,47 +16,6 @@ const cssVariableRgb = computed(() => {
 </template>
 
 <style scoped>
-/* .app-button {
-  cursor: pointer;
-  position: relative;
-  padding: 10px 24px;
-  font-size: 18px;
-  color: rgb(v-bind(cssVariableRgb));
-  border: 2px solid rgb(v-bind(cssVariableRgb));
-  background-color: rgb(var(--white-rgb), 0.2);
-  font-weight: 600;
-  transition: all 0.3s cubic-bezier(0.23, 1, 0.32, 1);
-  overflow: hidden;
-}
-
-.app-button::before {
-  content: '';
-  position: absolute;
-  inset: 0;
-  margin: auto;
-  width: 50px;
-  height: 50px;
-  border-radius: inherit;
-  scale: 0;
-  z-index: -1;
-  background-color: rgb(v-bind(cssVariableRgb));
-  transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
-}
-
-.app-button:hover::before {
-  scale: 3;
-}
-
-.app-button:hover {
-  color: rgb(var(--white-rgb));
-  scale: 1.1;
-  box-shadow: 0 0px 20px rgba(v-bind(cssVariableRgb), 0.4);
-}
-
-.app-button:active {
-  scale: 1;
-} */
-
 .app-button {
   display: flex;
   align-items: center;
